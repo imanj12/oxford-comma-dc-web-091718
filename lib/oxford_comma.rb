e232520343a2025a26b2c574fcf9be17656ad0e2
Iman Jawad <imanj12@gmail.com>
2018-09-06 16:13:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
+  string = ""
   if array.length == 1
     string = array.join
   elsif array.length == 2
     string = array.join(" and ")
   else
-    string = ""
     array.each_with_index do |ele, i|
       if i == array.length - 1
         string += "and #{ele}"
